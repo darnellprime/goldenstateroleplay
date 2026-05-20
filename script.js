d6996596f87159426c480dae5da0012a9a2e1ed9
darnellprime <rosediditlol@gmail.com>
2026-05-19 21:16:51 -0400

    S

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
 console.log("Golden State RP Loaded");
+
+/* NAVBAR SHADOW */
+
+window.addEventListener("scroll", () => {
+
+  const header = document.querySelector("header");
+
+  if(window.scrollY > 50){
+    header.style.background = "rgba(0,0,0,0.8)";
+  }
+
+  else{
+    header.style.background = "rgba(0,0,0,0.4)";
+  }
+
+});
